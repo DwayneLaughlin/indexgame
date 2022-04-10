6abde0ba346ff88b7350bbd350c452b0a9b6e7e9
DwayneL <dr2@ameritech.net>
2022-04-10 10:58:06 -0500

    added comments explaining everything in js file for Player class and added comment to html file explaining what <canvas> does

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+// Grabs the canvas tag from html file
 const canvas = document.querySelector('canvas');
+// context - returns an object that gives details for drawing images and graphics on a canvas
+const context = canvas.getContext('2d')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+// class - template for creating objects
+// This class creates templates for players
+class Player {
+    // constructor - adds properties to empty object created by the class
+    constructor(){
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.width = 30
+        this.height = 30
+    }
+    // draw = method
+    // fillstyle = changes styling
+    // firRect = creates rectanglt based on parameters of position on x and y axis as well as width and height of the rectangle
+    draw(){
+        context.fillStyle = 'red'
+        context.fillRect(this.position.x, this.position.y, this.width, this.height )
+    }
+}
+
+// 'new' keyword does 3 things
+// 1. creates an empty object
+// 2. sets the value of 'this' to be the new empty object
+// 3. calls the constructor method
+const player = new Player()
+player.draw()
