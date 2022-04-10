2ca5e28e9a464732b92dd88d254e2b19ed55ea1b
DwayneL <dr2@ameritech.net>
2022-04-10 11:38:16 -0500

    added animation function to give rectangle movement

--- index.js
+++ index.js
@@ -6,17 +6,26 @@ const context = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
+const gravity = 0.5
+
 // class - template for creating objects
 // This class creates templates for players
 class Player {
     // constructor - adds properties to empty object created by the class
     constructor(){
+        // position in window
         this.position = {
             x: 100,
             y: 100
         }
+        // velocity is needed for gravity. adding positive numbers will make you go down along y axis
+        this.velocity = {
+            x: 0,
+            y :1
+       }
         this.width = 30
         this.height = 30
+        
     }
     // draw = method
     // fillstyle = changes styling
@@ -25,6 +34,17 @@ class Player {
         context.fillStyle = 'red'
         context.fillRect(this.position.x, this.position.y, this.width, this.height )
     }
+
+    update(){
+        // draws rectangle
+        this.draw()
+        // creates gravity for rectangle by updating it's position and increasing it's fall rate gradually in the animate function
+        this.velocity.y += gravity
+        this.position.y += this.velocity.y
+        
+       
+
+    }
 }
 
 // 'new' keyword does 3 things
@@ -32,4 +52,13 @@ class Player {
 // 2. sets the value of 'this' to be the new empty object
 // 3. calls the constructor method
 const player = new Player()
-player.draw()
+
+// creates animation loop. requestAnimationFrame function takes callback that we want to loop
+// clearRect removes pixels in rectangular area
+function animate(){
+    requestAnimationFrame(animate)
+    context.clearRect(0,0,canvas.width,canvas.height)
+    player.update()
+};
+
+animate()
